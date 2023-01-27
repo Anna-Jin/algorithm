4d85c7cababced8b0166d4eb345c457de5428c9c
Anna-yujin <anna.jin@h2ohospitality.io>
2023-01-27 22:31:43 +0900

    나누어 떨어지는 숫자 배열

--- programmers/0127.py
+++ programmers/0127.py
@@ -9,6 +9,24 @@ def solution1(a, b):
     return answer
 
 print(solution1(5, 24))
+print("================================")
 
+# 나누어 떨어지는 숫자 배열
+def solution2(arr, divisor):
+    answer = []
 
+    for i in arr:
+        if i % divisor == 0:
+            answer.append(i)
 
+    if len(answer) == 0:
+        answer.append(-1)
+
+    answer.sort()
+
+    return answer
+
+arr = [3,2,6]
+divisor = 10
+print(solution2(arr, divisor))
+print("================================")
